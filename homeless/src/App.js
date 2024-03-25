b49a968621e9e4c2f4bcce9c80962407270bc10b
MangCau <namanhcute2809@gmail.com>
2024-03-25 14:15:45 +0700

    init

---
+++ homeless/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header/>
+      <h1>hihi</h1>
+    </div>
+  )
+}
+
+export default App;
