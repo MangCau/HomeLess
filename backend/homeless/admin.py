eca2fa58fc92281aa043e5543240dde2e6a77568
thbao4545 <101380018+thbao4545@users.noreply.github.com>
2024-05-08 18:05:47 +0700

    add requirement

--- backend/homeless/admin.py
+++ backend/homeless/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(SensorRecord)
+class ActivityLogAdmin(admin.ModelAdmin):
+    # Define the list display fields
+    list_display = ('type', 'status', 'timestamp')
+
+    # Define the ordering
+    ordering = ('-timestamp',)
+
+class SensorRecordAdmin(admin.ModelAdmin):
+    # Define the list display fields
+    list_display = ('sensor', 'value', 'timestamp')
+
+    # Define the ordering
+    ordering = ('-timestamp',)
+
+# Register your model with the custom admin class
+admin.site.register(SensorRecord, SensorRecordAdmin)
 admin.site.register(Light)
 admin.site.register(Fan)
 admin.site.register(Schedule)
-admin.site.register(ActivityLog)
+admin.site.register(ActivityLog, ActivityLogAdmin)
